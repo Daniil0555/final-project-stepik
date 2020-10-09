c31dd082b060f6c3a9d2c207df900e515269136b
Даниил Головин <d.golovin@city-mobil.ru>
2020-10-10 02:09:25 +0300

    add method should_be_login_url

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,13 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    INPUT_LOGIN = (By.NAME, "login-username")
+    INPUT_PASSWORD = (By.NAME, "login-password")
+    BUTTON_LOGIN = (By.NAME, "login_submit")
+    REGISTRATION_EMAIL = (By.NAME, "registration-email")
+    REGISTRATION_PASSWORD = (By.NAME, "registration-password1")
+    REGISTRATION_PASSWORD_REPLAY = (By.NAME, "registration-password2")
+    BUTTON_REGISTRATION = (By.NAME, "registration_submit")
